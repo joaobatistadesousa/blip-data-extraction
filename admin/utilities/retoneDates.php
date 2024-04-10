5b47997b21dc2aec6f56b1b87c8ce5ec31ce5ae6
joaobatistadesousa <joao.batista@itechit.com.br>
2024-04-10 16:20:42 -0300

    ajust date for 1 mounth

--- admin/utilities/retoneDates.php
+++ admin/utilities/retoneDates.php
@@ -4,7 +4,7 @@ function calcularDatas() {
     $endDate = date('Y-m-d');
 
     // Subtrair 94 dias da data atual para obter a start_date
-    $startDate = date('Y-m-d', strtotime('-94 days', strtotime($endDate)));
+    $startDate = date('Y-m-d', strtotime('-30 days', strtotime($endDate)));
 
     return array('start_date' => $startDate, 'end_date' => $endDate);
 }
