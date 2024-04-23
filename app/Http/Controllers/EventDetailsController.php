12dc4e19935d0f282390334c13986124f2e4054d
joaobatistadesousa <joao.batista@itechit.com.br>
2024-04-23 19:48:43 -0300

    remove  curl_setopt(, CURLOPT_CAINFO, path)

--- app/Http/Controllers/EventDetailsController.php
+++ app/Http/Controllers/EventDetailsController.php
@@ -185,7 +185,7 @@ class EventDetailsRequest
         curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_CAINFO, 'C:/laragon/etc/ssl/cacert.pem');
+        // curl_setopt($ch, CURLOPT_CAINFO, 'C:/laragon/etc/ssl/cacert.pem');
 
         // Adicione estes logs para debugar
         try {
--- app/Http/Controllers/SentMessageController.php
+++ app/Http/Controllers/SentMessageController.php
@@ -98,7 +98,7 @@ class SentMessageController extends Controller
            curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-           curl_setopt($ch, CURLOPT_CAINFO, 'C:/laragon/etc/ssl/cacert.pem');
+        //    curl_setopt($ch, CURLOPT_CAINFO, 'C:/laragon/etc/ssl/cacert.pem');
 
            try {
                $response = curl_exec($ch);
